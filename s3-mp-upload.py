16be33f88afc9f2682a27bd2ffdcdedc113dfc90
zgbsfs <zgbsfs@hotmail.com>
2015-09-19 03:30:28 +0800

    concurrerency simulate

--- s3-mp-upload.py
+++ s3-mp-upload.py
@@ -13,8 +13,8 @@ import boto
 from boto.s3.connection import S3Connection
 import random
 import sys, traceback
-from scipy.stats import poisson
 import signal
+
 parser = argparse.ArgumentParser(description="Transfer large files to S3",
         prog="s3-mp-upload")
 parser.add_argument("src", type=file, help="The file to transfer")
@@ -30,6 +30,7 @@ parser.add_argument("--insecure", dest='secure', help="Use HTTP for connection",
 parser.add_argument("-t", "--max-tries", help="Max allowed retries for http timeout", type=int, default=5)
 parser.add_argument("-v", "--verbose", help="Be more verbose", default=False, action="store_true")
 parser.add_argument("-q", "--quiet", help="Be less verbose (for use in cron jobs)", default=False, action="store_true")
+parser.add_argument("-S", "--simulate", help="Enable simulation with the time per error ",type=int ,default=0)
 logger = logging.getLogger("s3-mp-upload")
 
 def handler(signum, frame):
@@ -61,7 +62,7 @@ def do_part_upload(args):
     s3.is_secure = secure
     bucket = s3.get_bucket(bucket_name)
     mpu = None
-    
+    print "which process  " +str(i) 
     for mp in bucket.list_multipart_uploads():
         if mp.id == mpu_id:
             mpu = mp
@@ -81,18 +82,18 @@ def do_part_upload(args):
     try:
         # Do the upload
         t1 = time.time()
-	mpu.upload_part_from_file(StringIO(data), i+1)
+	mpu.upload_part_from_file(StringIO(data), i)
         t2 = time.time() - t1
 	#print t2
         s = len(data)/1024./1024.
-	logger.info("Uploaded part %s (%0.2fM) in %0.3f s at %0.2f MBps" % (i+1, s, t2, s/t2))
+	logger.info("Uploaded part %s (%0.2fM) in %0.3f s at %0.2f MBps" % (i, s, t2, s/t2))
     except Exception, err:
 	#traceback.print_exc()
 	logger.info(err)
         logger.info("Retry request %d of max %d times" % (current_tries, max_tries))
         do_part_upload(args)
 
-def main(src, dest, num_processes=2, split=50, force=False, reduced_redundancy=False, verbose=False, quiet=False, secure=False, max_tries=5):
+def main(src, dest, num_processes=2, split=50, force=False, reduced_redundancy=False, verbose=False, quiet=False, secure=False, max_tries=5, simulate=120):
     # Check that dest is a valid S3 url
     split_rs = urlparse.urlsplit(dest)
     if split_rs.scheme != "s3":
@@ -104,13 +105,7 @@ def main(src, dest, num_processes=2, split=50, force=False, reduced_redundancy=F
        sp=160
     s3 = S3Connection()
     s3.is_secure = secure
-    '''
-    print split_rs
-    print s3.get_all_buckets()
-    print "\n"	
-    print s3.lookup("eu-west-1-lin")
-    print s3.get_bucket("eu-central-1-lin")
-    '''
+
     bucket = s3.get_bucket(split_rs.netloc)
     if bucket == None:
         raise ValueError("'%s' is not a valid bucket" % split_rs.netloc)
@@ -124,7 +119,6 @@ def main(src, dest, num_processes=2, split=50, force=False, reduced_redundancy=F
     src.seek(0,2)
     size = src.tell()
     num_parts = int(ceil(size / part_size))
-    print num_parts
     # If file is less than 5M, just upload it directly
     if size < 5*1024*1024:
         src.seek(0)
@@ -144,33 +138,43 @@ def main(src, dest, num_processes=2, split=50, force=False, reduced_redundancy=F
     mpu = bucket.initiate_multipart_upload(split_rs.path, reduced_redundancy=reduced_redundancy)
     logger.info("Initialized upload: %s" % mpu.id)
     # Generate arguments for invocations of do_part_upload
-    def gen_args(x,num_parts, fold_last):
-        for i in range(num_parts+1-x):
-	    i+=x
-            part_start = part_size*i
-            if i == (num_parts-1) and fold_last is True:
+    def gen_args(x, fold_last, upload_list):
+	print upload_list
+        for i in upload_list:
+            part_start = part_size*(i-1)
+            if i == num_parts and fold_last is True:
                 yield (bucket.name, mpu.id, src.name, i, part_start, part_size+10*1024*1024, secure, max_tries, 0)
-                break
+	    elif upload_list==None:
+		print "finish  \n " +str(upload_list)
+		break
             else:
                 yield (bucket.name, mpu.id, src.name, i, part_start, part_size, secure, max_tries, 0)
+	    '''
+	    finished_list=mpu.get_all_parts()
+	    for i in finished_list:
+                a=int(str(i).split(" ")[1].split(">")[0])
+            #    print "what to del " + str(a)
+		if a in upload_list:
+	                upload_list.remove(a)
 
+	#	print upload_list
+            '''
     # If the last part is less than 5M, just fold it into the previous part
     fold_last = ((size % part_size) < 10*1024*1024)
+    upload_list=[]
+    for i in range(1,num_parts+1):   
+	    upload_list.append(i)
     # Do the thing
     t1 = time.time()
-    def master_progress(mpu,num_processes,bucket,num_parts):
+    def master_progress(mpu,num_processes,bucket,upload_list):
 	    x=0
 	    while True:
 		try:
 			if x!=num_parts:
-				print x
-				print num_parts
 				pool = Pool(processes=num_processes)
-				pool.map_async(do_part_upload, gen_args(x,num_parts, fold_last)).get(99999999)
-			else:
-				continue
+				pool.map_async(do_part_upload, gen_args(x,fold_last,upload_list)).get(99999999)
                         src.close()
-                        print mpu.get_all_parts()
+			print "finish" +str(mpu.get_all_parts())
                         mpu.complete_upload()
                         hdlr = logging.FileHandler("bigmultipart.log")
                         formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
@@ -188,21 +192,30 @@ def main(src, dest, num_processes=2, split=50, force=False, reduced_redundancy=F
 			break
 		except Exception, err:
 			logger.error("Encountered an error, canceling upload aaaaaaaaaaaa")
-			traceback.print_exc()
+			pool.terminate()
+	#		traceback.print_exc()
 			print mpu.get_all_parts()
 			x= len(mpu.get_all_parts())
-			print x
-			pool.terminate()
+			for i in mpu.get_all_parts():
+				a=int(str(i).split(" ")[1].split(">")[0])
+				#    print "what to del " + str(a)
+				if a in upload_list:
+					upload_list.remove(a)
+			print "exception :upload " +str(x) +"parts"
 			logger.error(err)
-			signal.alarm(int(random.expovariate(1.0/120.0)))
+			if simulate!=0:
+				#signal.alarm(int(random.expovariate(1.0/120.0)))
+				signal.alarm(7)
 			#break
+
     t1 = time.time()
     #print mpu.get_all_parts()
     signal.signal(signal.SIGALRM, handler)
-    signal.alarm(int(random.expovariate(1.0/120.0)))
-#    signal.alarm(15)
-    master_progress(mpu,num_processes,bucket,num_parts)
-#    mpu.complete_upload()
+    print simulate
+    if simulate!=0:
+    	signal.alarm(int(random.expovariate(1.0/simulate)))
+   #    signal.alarm(10)
+    master_progress(mpu,num_processes,bucket,upload_list)
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
